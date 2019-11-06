f6c9b0fd1eaadaf45535887dac0566acae10964c
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-11-06 21:36:31 +0000

    Added current files

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    console.log("Runnning on Ready");
+    
+    //*********************************************************************
+    // Functions for the customer section
+    $("main .customerHistoryButton").on("click", function(e) {
+        window.location = "customerHistory.html";
+    });
+    
+    $("main .customerScheduleButton").on("click", function(e) {
+        window.location = "customerSchedule.html";
+    });
+    
+    $("main .estimatorButton").on("click", function(e) {
+        window.location = "https://parabug.xyz/";
+    });
+    
+    $("main .customerFeedbackButton").on("click", function(e) {
+        window.location = "customerFeedback.html";
+    });
+    
+    $("main .customerContactButton").on("click", function(e) {
+        window.location = "https://www.parabug.solutions/contact-us";
+    });
+    
+    // TODO: Add Logout functionality once Login is implemented
+    $("footer .customerLogOutButton").on("click", function(e){
+        window.location = "index.html";
+    });
+    //*********************************************************************
+});
