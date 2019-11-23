50b5b8616001adae6bf727addbbed45df2e27b70
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-11-23 01:33:50 +0000

    ok

--- insert.php
+++ insert.php
@@ -1,16 +1,7 @@
 <?php
 include 'dbConnection.php';
-
-// session_start();
-//     $httpMethod = strtoupper($_SERVER['REQUEST_METHOD']);
-//     switch($httpMethod) {
-        
-//         case 'POST':
-//             break;
-        
-//     }
-
 $conn = getDatabaseConnection("heroku_68533dd666c4a97");
-$sql = "INSERT INTO `operator` (`opUsername`) VALUES ('JOsh');";
+$fav = $_GET["iconfav"];
+$sql = "INSERT INTO `operator` (`opUsername`) VALUES ('JoshJepson');";
 $conn->query($sql);
 ?>
