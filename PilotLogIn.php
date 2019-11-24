b23780a139a062257d7b374e63fedd0bf36a71a9
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-11-24 06:55:38 +0000

    change pilot

--- PilotLogIn.php
+++ PilotLogIn.php
@@ -1,18 +1,73 @@
-  <?php
+<?php
+session_start();
+ 
+  if(isset($_POST['login'])){
+    include 'dbConnection.php';
+    $conn = getDatabaseConnection("heroku_68533dd666c4a97");
+    $username = $_POST['opUsername'];
+    $password = sha1($_POST['opPassword']);
+    $sql = "SELECT * FROM operator
+                 WHERE username = :opUsername
+                 AND  password = :opPassword";                 
+    $port = array();
+    $port[':opUsername'] = $username;
+    $port[':opPassword'] = $password;
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($port);
+    $record = $stmt->fetch(PDO::FETCH_ASSOC); //we're expecting just one record
+    if (empty($record)) {
+    
+    echo " <div class = 'errorMessage'> ";
+    echo "</br></br></br>Wrong username or password!" ;
+    echo "</div>";
+    } else {
+   
+   //$_SESSION['adminFullName'] = $record['firstName'] .  "   "  . $record['lastName'];
+   //changed here because we are not sure why it's here.
+   header('Location: PilotHome.php'); //redirects to another program
+    
+  }
+ }
 ?>
+
+<!DOCTYPE html>
 <html>
     <head>
-        <title> Pilot Log In </title>
+        <title> Pilot Login </title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
+
+
     </head>
-    <body>
-    <h1> Pilot Log In</h1>
-    Username: 
-    <input type="text">
-    <br> <br>
-    Password:
-    <input type= "password"> <br>
-    
-    <button> Log In</button>
-    
+        <header>
+
+     <h1> Pilot Login </h1>
+   
+     </header>
+    <body >
+
+        <form method="post">
+          Username:  <input type="text" name="username"/> <br><br>
+          Password:  <input type="password" name="password"/> <br><br>
+          <input type="submit" value="Login" name = "login" id="b1">
+        </form>
     </body>
 </html>
+
+<!--// <html>-->
+<!--//     <head>-->
+<!--//         <title> Pilot Log In </title>-->
+<!--//     </head>-->
+<!--//     <body>-->
+<!--//     <h1> Pilot Log In</h1>-->
+<!--//     Username: -->
+<!--//     <input type="text">-->
+<!--//     <br> <br>-->
+<!--//     Password:-->
+<!--//     <input type= "password"> <br>-->
+    
+<!--//     <button> Log In</button>-->
+    
+<!--//     </body>-->
+<!--// </html>-->
