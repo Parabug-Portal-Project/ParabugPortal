0a07a2c8bd7269e0ae0682c28a05a2936fcfec29
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-08 08:22:47 +0000

    new

--- PilotInfo.php
+++ PilotInfo.php
@@ -1,42 +1,32 @@
-  <?php
+<?php
+    include 'dbConnection.php';
+    $conn = getDatabaseConnection("heroku_68533dd666c4a97");
+ { 
+     global $conn;
+     
+     $sql = "SELECT * FROM operator";
+     $stmt = $conn->prepare($sql);
+     $stmt->execute();
+     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+     
+     foreach ($records as $record) {
+         echo "<td>".$record['idOperator'] . " </td><td>".$record['opProfilepicture'] . " </td><td> " . $record['opEmail'] . "</td> 
+         <td>".$record['opAddress'] . " </td> <td>".$record['opInsurance'] . " </td>
+         <td>".$record['opFaawaver'] . " </td><td>".$record['opLicense'] . " </td><td>".$record['opRegistration'] . " </td>
+         <td>".$record['opW9'] . " </td><td>".$record['opVendor'] . " </td>";
+     }
+     echo "</br>";
+     echo "</br>";
+ }
 ?>
+
+
 <html>
     <head>
-        <title> Pilot Info </title>
+        <title> Pilot Job Info </title>
         <link href="css/styles2.css" rel="stylesheet" type="text/css" />
     </head>
-    <body>
-     <header>
-            <table class="menuHeader">
-                <tr><td><strong>Pilot Portal</strong></td></tr>
-            </table>
             <div class="logo">
                 <img src="img/Parabug-Logo.png"></img>
-            </div>
-        </header>
-
- <main>
-            
-            <!--Created this table to separate each button into it's own row-->
-            <table class="customerMenu">
-                <!--Ignore this-->
-                <tr><td></td></tr><tr><td></td></tr><tr><td></td></tr>
-                 <tr><td class="customerMenuLayout"><div class="piStandard"><strong>PILOT INFO</strong></div></td></tr>
-                <tr><td class="customerMenuLayout"><button id="ii" class="plo"><strong>INSURANCE INFO</strong></button></td></tr>
-                <tr><td class="customerMenuLayout"><button id="p107"class="plo"><strong>PART 107 LICENSE</strong></button></td></tr>
-                <tr><td class="customerMenuLayout"><button id="faaw"class="plo"><strong>FAA WAIVERS</strong></button></td></tr>
-                <tr><td class="customerMenuLayout"><button id="ra"class="plo"><strong>REGISTERED AIRCRAFT</strong></button></td></tr>
-                <tr><td class="customerMenuLayout"><button id="rp"class="plo"><strong>REGISTERED PARABUG</strong></button></td></tr>
-                <!--<tr><td></td></tr><tr><td></td></tr><tr><td></td></tr>-->
-                <!--<tr><td class="customerMenuLayout"><button class="customerLogOutButton"><strong>LOG OUT</strong></button></td></tr>-->
-            </table>
-        </main>
-
- 
- <footer class="customerFooter">
-            <div class="logoutDiv">
-                <button class="plo"><strong>LOG OUT</strong></button>
-            </div>
-        </footer>
-    </body>
+      </div>
 </html>
--- customerHistory.php
+++ customerHistory.php
@@ -1,66 +1,31 @@
-<?php?>
+<?php
+    include 'dbConnection.php';
+    $conn = getDatabaseConnection("heroku_68533dd666c4a97");
+ { 
+     global $conn;
+     
+     $sql = "SELECT * FROM job";
+     $stmt = $conn->prepare($sql);
+     $stmt->execute();
+     $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+     
+     foreach ($records as $record) {
+         echo "<td>".$record['joID'] . " </td><td>".$record['joDate'] . " </td><td> " . $record['joLocation'] . "</td> 
+         <td>".$record['joStatus'] . " </td> <td>".$record['idCustomer'] . " </td>
+         <td>".$record['idOperator'] . " </td>";
+     }
+     echo "</br>";
+     echo "</br>";
+ }
+?>
+
+
 <html>
     <head>
-        <meta charset="utf-8" />
-        <title> Parabug Portal </title>
-        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <title> Pilot Job History </title>
+        <link href="css/styles2.css" rel="stylesheet" type="text/css" />
     </head>
-    
-    <body>
-
-        <header>
-            <table class="menuHeader">
-                <tr><td><strong>Customer Portal</strong></td></tr>
-                <!--Ignore this-->
-            </table>
-            <br/> <br/>
-            <table class="historyHeader">
-                <tr>
-                    <td><strong>HISTORY</strong></td>
-                </tr>
-            </table>
             <div class="logo">
                 <img src="img/Parabug-Logo.png"></img>
-            </div>
-        </header>
-    
-        <main>
-            <table>
-                <th>
-                    <td>
-                        <strong>DATE</strong>
-                    </td>
-                    <td>
-                        <strong>FIELD SIZE</strong>
-                    </td>
-                    <td>
-                        <strong>INSECT / MEDIA TYPE</strong>
-                    </td>
-                    <td>
-                        <strong>LOCATION</strong>
-                    </td>
-                    <td>
-                        <strong>PILOT INFO</strong>
-                    </td>
-                    <td>
-                        <strong>PICTURES</strong>
-                    </td>
-                </th>
-                <tr>
-                    <td></td>
-                </tr>
-            </table>
-            
-            
-            
-        </main>
-        
-        <footer class="customerFooter">
-            <br>
-            <button class="customerLogOutButton"><strong>LOG OUT</strong></button>
-        </footer>
-    
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script src="js/functions.js"></script>
-    </body>
+      </div>
 </html>
