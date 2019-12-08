e085598c82462aefa9391cd09290fc6064245312
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-08 21:59:37 +0000

    new

--- inc/functions.php
+++ inc/functions.php
@@ -1,4 +1,24 @@
 <?php
+function displayAllJobs(){
+    global $conn;
+    
+    $sql = "SELECT * FROM job";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //we're expecting multiple records
+    foreach ($records as $record) {
+        echo "Job Date: " . $record[joDate] . "</br>";
+        echo "Job Location: " . $record[joLocation] . "</br>";
+        echo "<a class='btn btn-warning' role='button' href='updateJob.php?joId=".$record['joId']."'>Update</a>";
+        //echo "[<a href='deleteJob.php?joId=".$record['joId']."'>Delete</a>]";
+        echo "<form action='deleteJob.php' onsubmit='return confirmDelete()'>";
+        echo "   <input type='hidden' name='joId' value='".$record['joId']."'>";
+        echo "   <button class='btn btn-outline-danger' type='submit'>Delete</button>";
+        echo "</form>";
+        echo "</br></br>";
+        
+    }
+}
 function getId($jobId) {
      global $conn;
     
---
+++ jobHistory.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include 'dbConnection.php';
+$conn = getDatabaseConnection("heroku_68533dd666c4a97");
+include 'inc/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Job History </title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" type="text/css" />
+        
+        <script>
+        
+            function confirmDelete() {
+                
+                //alert();
+                //prompt();
+                return confirm("Are you sure you want to delete this product");
+                
+            }
+            
+        </script>
+    
+    </head>
+    <body id= "adminBody">
+        
+        
+        <?= displayAllJobs() ?>
+ 
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        
+    </body>
+</html>
