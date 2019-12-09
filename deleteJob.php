bba45a6b0f534783607129329fb4ff6fb6a0fe37
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-09 23:06:17 +0000

    trying to update stuff

--- deleteJob.php
+++ deleteJob.php
@@ -6,5 +6,6 @@ $conn = getDatabaseConnection("heroku_68533dd666c4a97");
 $sql = "DELETE FROM job WHERE jobId = " . $_GET['jobId'];
 $stmt=$conn->prepare($sql);
 $stmt->execute();
+header("Location: jobHistory.php");
 
 ?>
