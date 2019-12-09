f9725268d0bad696adc39e18accd15b8e60a65e5
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-09 00:56:08 +0000

    changing the way pilot home works

--- pilotHome.php
+++ pilotHome.php
@@ -29,8 +29,6 @@
             <table class="customerMenu">
                 <!--Ignore this-->
                 <tr><td></td></tr><tr><td></td></tr><tr><td></td></tr>
-                
-                <tr><td class="customerMenuLayout"><button class="pi"><strong>PILOT INFO</strong></button></td></tr>
                 <tr><td class="customerMenuLayout"><button class="ph"><strong>JOB HISTORY</strong></button></td></tr>
                 <tr><td class="customerMenuLayout"><button class="ps"><strong>SCHEDULE</strong></button></td></tr>
                 <tr><td class="customerMenuLayout"><button class="pe"><strong>ESTIMATOR</strong></button></td></tr>
@@ -50,15 +48,13 @@
  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
     <script>
-        $("button.pi").on("click", function(e) {
-            window.location = "pilotInfo.php";
-        });
+
         $("button.ph").on("click", function(e) {
             window.location = "pilotHistory.php";
         });
         
         $("button.ps").on("click", function(e) {
-            window.location = "pilotSchedule.php";
+            window.location = "https://accounts.google.com/signin/v2/identifier?service=cl&passive=1209600&osid=1&continue=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender&followup=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender&scc=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
         });
         $("button.pe").on("click", function(e) {
             window.location = "https://parabug.xyz/";
