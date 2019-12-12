a7a16b4b723d5c869443b81f731f2088ea4ec98e
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-12 21:29:02 +0000

    Index Edit

--- index.php
+++ index.php
@@ -4,27 +4,16 @@
 <head>
     <meta charset="utf-8" />
     <title> Parabug Portal </title>
-    <link href="css/styles.css" rel="stylesheet" type="text/css" /></head>
+    <link href="css/styles2.css" rel="stylesheet" type="text/css" /></head>
 <body>
-    
-    <header>
-        <div class="mainMenuHeader">
-            <strong>Please Log In</strong>
-        </div>
-        <div class="logo">
-            <img src="img/Parabug-Logo.png"></img>
-        </div>
-    </header>
-    
-    <main>
-        <table class="mainMenuButtons">
-            <tr>
-                <td class="loginButtons"><button class="adminLoginButton"><strong>Admin Login In</strong></button></td>
-                <td class="loginButtons"><button class="customerLoginButton"><strong>Customer Login In</strong></button></td>
-                <td class="loginButtons"><button class="pilotLoginButton"><strong>Pilot Login In</strong></button></td>
-            </tr>
-        </table>
-    </main>
+    <center>
+<img src ="img/logo.png" style="width: 75%; height: 75%"/>
+Please Log In <br>
+ <button class="adminLoginButton"><strong>Admin Login In</strong></button></td>
+ <button class="customerLoginButton"><strong>Customer Login In</strong></button></td>
+<button class="pilotLoginButton"><strong>Pilot Login In</strong></button></td>
+          
+    </center>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
