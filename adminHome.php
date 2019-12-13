600ad1ab82d702e2d0fc82764d45a791525b0adf
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-13 00:02:41 +0000

    Index Edit

--- adminHome.php
+++ adminHome.php
@@ -8,14 +8,21 @@
     </head>
     <body>
     Admin Home
+    <tab>
         <form action="createJob.php">
               <input type="submit" value="Create Job">
           </form>
+          
+          </tab>
+          <tab>
        <form action="createOperator.php">
               <input type="submit" value="Create Operator">
           </form>
+          </tab>
+          <tab>
         <form action="createCustomer.php">
               <input type="submit" value="Create Customer">
           </form>
+          </tab>
     </body>
 </html>
