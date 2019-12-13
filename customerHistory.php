ec757deef26533016cc2305ea2a3be6b98cb89d5
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-13 00:59:23 +0000

    Index Edit

--- customerHistory.php
+++ customerHistory.php
@@ -25,8 +25,8 @@
                         
                         // Hard coded for now, hoping to show only the Jobs that have the same customer ID as
                         // the current user soon!
-                        // $sql = "SELECT * FROM job WHERE idCustomer=" . 1234;
-                        $sql = "SELECT * FROM job JOIN operator WHERE idCustomer=" . 1234;
+                        $sql = "SELECT * FROM job WHERE idCustomer=" . 1234;
+                        // $sql = "SELECT * FROM job JOIN operator WHERE idCustomer=" . 1234;
                         $stmt = $conn->prepare($sql);
                         $stmt->execute();
                         $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -34,11 +34,9 @@
                         
                         foreach ($records as $record) {
                             echo "<tr><td>".$record['idJob'] . "</td><td>" . $record['joDate'] . " </td><td> " . $record['joLocation'] . "</td><td>"
-                            . (($record['joStatus'] == 0) ? "Not Completed" : "Completed") . " </td><td>" . $record['opName'] . " </td></tr>";
+                            . (($record['joStatus'] == 0) ? "Not Completed" : "Completed") . " </td><td>" . $record['idOperator'] . " </td></tr>";
                         }
-                        
-                        echo $print_r($records);
-                    }
+                                            }
                 ?>
             
             </table>
