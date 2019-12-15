0c0fab7ee3715562a76afc6d140649725cb88ce3
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-15 21:41:06 +0000

    new

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php?>
 <html>
 
+
+
+
+
 <head>
     <meta charset="utf-8" />
     <title> Parabug Portal </title>
     <link href="css/styles2.css" rel="stylesheet" type="text/css" /></head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <body>
     <center>
+<div class="container">
+  <div class="jumbotron">
 <img src ="img/logo.png" class ="logo"/>
 <br> <br>
 Please Log In <br>
@@ -15,6 +24,8 @@ Please Log In <br>
 <button class="pilotLoginButton"><strong>Pilot Login</strong></button></td>
           
     </center>
+</div>
+
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
