3099cd34aadc3e82083f4bbd21abf1933a6168c2
EC2 Default User <ec2-user@ip-172-31-19-73.us-east-2.compute.internal>
2019-12-18 08:10:30 +0000

    Styling

--- adminHome.php
+++ adminHome.php
@@ -7,28 +7,29 @@
           <link href="css/styles2.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-<center>
-<img src ="img/logo.png" class ="logo"/>
-<br> <br>
-<div class="one"> Admin Home</div>
-  <br> <br>
-  <div class="flex">
-        <form action="createJob.php">
-              <input type="submit" value="Create Job">
+      <center>
+      <img src ="img/logo.png" class ="logo"/>
+      <br> <br>
+      
+      <div class="one"> Admin Home</div>
+        <br> <br>
+        <div class="flex">
+          <form action="createJob.php">
+            <input type="submit" value="Create Job">
           </form>
           
-       <form action="createOperator.php">
+          <form action="createOperator.php">
               <input type="submit" value="Create Operator">
           </form>
-        
-        <form action="createCustomer.php">
+          
+          <form action="createCustomer.php">
               <input type="submit" value="Create Customer">
           </form>
-        
-        <form action="jobHistory.php">
+          
+          <form action="jobHistory.php">
               <input type="submit" value="Job History">
           </form>          
-          </div>
-          </center>
+        </div>
+        </center>
     </body>
 </html>
--- customerHome.php
+++ customerHome.php
@@ -8,32 +8,22 @@
     
     <body>
 
-        <header>
-            <table class="menuHeader">
-            </table>
-            <br/> <br/>
-    
-        </header>
+        <center>
+            <div class="flex">
+                <form action="customerHistory.php">
+                    <input type="submit" value="Customer History"/>
+                </form>
+                
+                <form action="pilotInfo.php">
+                    <input type="submit" value="Pilot Info"/>
+                </form>
+                
+                <form>
+                    <input type="submit" value="Scheduler"/>
+                </form>
+            </div>
+        </center>
         
-     
-        <div class="flex">
-             <form action="customerHistory.php">
-                 <input type="submit" value="Customer History"/>
-             </form>
-             
-             <form action="pilotInfo.php">
-                 <input type="submit" value="Pilot Info"/>
-             </form>
-             
-             <form>
-                 <input type="submit" value="Scheduler"/>
-             </form>
-        </div>
-     
-        <!--<button class="customerHistoryButton"><strong>VIEW JOB</strong></button></td></tr>-->
-        <!--<button class="customerPilotButton"><strong>PILOT INFO</strong></button></td></tr>-->
-        <!--<button class="customerScheduleButton"><strong>SCHEDULE</strong></button></td></tr>-->
-
     
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="js/functions.js"></script>
